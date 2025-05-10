958ce59ce16ea6d9d6d56bfcf89072ca3d0da63f
SOV710 <chris916911179@outlook.com>
2025-05-11 04:16:17 +0800

    fix: remove upload function from frontend project

--- frontend/src/components/BaseHeader.vue
+++ frontend/src/components/BaseHeader.vue
@@ -74,23 +74,6 @@ function handleSuggestion() {
 
     <el-menu-item @click="handleSuggestion"> Suggest </el-menu-item>
 
-    <el-upload
-      ref="upload"
-      class="upload-demo"
-      action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-      :limit="1"
-      :on-exceed="handleExceed"
-      :auto-upload="false"
-    >
-      <template #trigger>
-        <el-button type="primary">select file</el-button>
-      </template>
-      <el-button class="ml-3" type="success" @click="submitUpload"> upload to server </el-button>
-      <template #tip>
-        <div class="el-upload__tip text-red">limit 1 file, new file will cover the old file</div>
-      </template>
-    </el-upload>
-
     <el-menu-item h="full" @click="toggleDark()">
       <el-button type="text" class="w-full" style="height: var(--el-menu-item-height)">
         <template #icon>
